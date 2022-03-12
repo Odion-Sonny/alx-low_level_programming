6573bb3876f3c96e3d9df8782e105fad35bc13a5
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-12 21:59:14 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main-entry point
+*Return: 0 if success
+*/
+
+int main(void)
+{
+int ch;
+for (ch= ‘a’; ch<= ‘z’; ch++)
+putchar(ch);
+putchar (‘\n’);
+getch();
+}
+
+
