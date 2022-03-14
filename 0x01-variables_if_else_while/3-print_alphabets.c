87ec187d996b7ea1293bddbfcaa16ec0d634e7bc
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-14 07:42:22 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main - function to print alphabet
- *
- * Return: no return
- *
- */
+* main - function to print alphabet
+*
+* Return: no return
+*
+*/
  
-int main(void)
+int main (void)
 {
 char i;
-for(i = 'a'; i <= 'z'; i++)
-putchar(i);
+for (i = 'a'; i <= 'z'; i++)
+putchar (i);
 
-for(i = 'A'; i <= 'Z'; i++)
-putchar(i);
-putchar('\n');
+for (i = 'A'; i <= 'Z'; i++)
+putchar (i);
+putchar ('\n');
 return (0);
 }
