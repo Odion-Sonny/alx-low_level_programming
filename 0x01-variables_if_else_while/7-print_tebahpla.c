954136d855c94096ce185b245b17e0106d7e12d0
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-14 08:28:01 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - function to print alphabet
+* Return: no return
+*/
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar (i);
+putchar ('\n');
+return (0);
+}
