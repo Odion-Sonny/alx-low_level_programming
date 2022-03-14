7e5cde6b1664d63f1c9c36eb132023bd8db804ea
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-14 08:23:03 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - function to print alphabet
+* Return: no return
+*/
+int main()
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+}
+putchar('\n');
+return (0);
+}
