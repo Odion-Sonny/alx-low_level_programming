0ecfbf61ff62b081ebeaf79ce4c3f0f149c15c21
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-14 08:11:41 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - function to print alphabet
+* Return: no return
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf ("%d", a);
+}
+printf ("\n");
+return (0);
+}
