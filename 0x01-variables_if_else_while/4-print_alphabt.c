68685441cf38149f9fe64a0fa58f41c23b99de5c
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-14 08:00:48 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - function to print alphabet
+* Return: no return
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == 'e' || i == 'q')
+putchar ('\b');
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
