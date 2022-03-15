5a282faa1d7556c9aca6daa454bd9156ba406355
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-16 00:56:02 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,13 +11,19 @@ int print_sign(int n)
 {
 int ret = 0;
 if (n > 0)
+{
 ret = 1;
 printf("+");
+}
 else if (n == 0)
+{
 ret = 0;
 printf("0");
+}
 else
+{
 ret = -1;
 printf("-");
-return (ret)
+}
+return (ret);
 }
