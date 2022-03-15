a7a6a6d3ddca46ddc114db0c23121ee91c692a98
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-15 23:47:38 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
+#include "main.h"
 #include <stdio.h>
 /**
-*main - prints _putchar
-*Return :0
-*/
+ * main - prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-printf('_putchar \n');
+// 	_putchar('_');
+// 	_putchar('p');
+// 	_putchar('u');
+// 	_putchar('t');
+// 	_putchar('c');
+// 	_putchar('h');
+// 	_putchar('a');
+// 	_putchar('r');
+// 	_putchar('\n');
+printf('_putchar \n')
 return (0);
-}  
+}
