a16a35d3b5cdef72678b188c78e68b74ae4895f0
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-16 00:24:25 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - print alphabet
+*Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char ch;
+int c;
+while (c = 10; count >= 0; c--)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar (ch);
+}
+putchar ('\n');
+}
