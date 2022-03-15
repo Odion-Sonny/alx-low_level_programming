53b9a3a2eab0bf31dcdf230a58b6614ace70a918
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-16 00:28:32 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,12 @@ void print_alphabet_x10(void)
 {
 char ch;
 int c;
-while (c = 10; count >= 0; c--)
+c = 0;
+while (c <= 10)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar (ch);
+c++
 }
 putchar ('\n');
 }
