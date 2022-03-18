45810fcbff519143072933d4ffd32d9a63103c29
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-03-18 22:24:14 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print 0 to 9 jump 2 and 4
+*
+*
+*/
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	if ((i == '2') || (i == '4'))
+    i++;
+  else
+	_putchar(i);
+	}
+	_putchar('\n');
+
+}
